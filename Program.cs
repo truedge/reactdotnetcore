36f9da656522be69882c5eddea02f171916a8a81
truedge <jamie@edgesoftware.tech>
2020-04-15 15:49:50 -0400

    update sec vun

--- Program.cs
+++ Program.cs
@@ -19,6 +19,7 @@ namespace reactdotnetcore
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseKestrel();
     }
 }
